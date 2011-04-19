94678c4cdb4429c3e2257619dbc0473da7504b5b
sam <sam@1433025c-d204-0410-b9d4-f0ece44ced3d>
2011-04-19 22:39:13 +0000

    core: import matrix.h from the Lol Engine. It's going to be useful.

---
+++ src/common.h
@@ -0,0 +1,18 @@
+/*
+ *  Abuse - dark 2D side-scrolling platform game
+ *  Copyright (c) 1995 Crack dot Com
+ *  Copyright (c) 2005-2011 Sam Hocevar <sam@hocevar.net>
+ *
+ *  This software was released into the Public Domain. As with most public
+ *  domain software, no warranty is made or implied by Crack dot Com or
+ *  Jonathan Clark.
+ */
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include "lol/matrix.h"
+using namespace lol;
+
+#endif // __COMMON_H__
+
