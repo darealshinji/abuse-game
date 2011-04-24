050a3757ee0803b659bfc88856967af6f36ce217
sam <sam@1433025c-d204-0410-b9d4-f0ece44ced3d>
2011-04-24 08:33:50 +0000

    tool: implement `get' in abuse-tool.

--- abuse/trunk/src/tool/abuse-tool.cpp
+++ abuse/trunk/src/tool/abuse-tool.cpp
@@ -19,6 +19,19 @@
 
 static void Usage();
 
+enum
+{
+    CMD_INVALID,
+    CMD_LIST,
+    CMD_GET,
+    CMD_MOVE,
+    CMD_DEL,
+    CMD_PUT,
+    CMD_RENAME,
+    CMD_GETPCX,
+    CMD_PUTPCX,
+};
+
 int main(int argc, char *argv[])
 {
     if (argc < 3)
@@ -27,22 +40,81 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    char *file = argv[1];
-    char *cmd = argv[2];
+    int cmd = !strcmp(argv[2], "list") ? CMD_LIST
+            : !strcmp(argv[2], "get") ? CMD_GET
+            : !strcmp(argv[2], "del") ? CMD_DEL
+            : !strcmp(argv[2], "put") ? CMD_PUT
+            : !strcmp(argv[2], "move") ? CMD_MOVE
+            : !strcmp(argv[2], "rename") ? CMD_RENAME
+            : !strcmp(argv[2], "getpcx") ? CMD_GETPCX
+            : !strcmp(argv[2], "putpcx") ? CMD_PUTPCX
+            : CMD_INVALID;
 
-    if (!strcmp(cmd, "list"))
+    if (cmd == CMD_INVALID)
     {
-        jFILE fp(file, "rb");
-        if (fp.open_failure())
-        {
-            fprintf(stderr, "ERROR - could not open %s\n", file);
-            return EXIT_FAILURE;
-        }
+        fprintf(stderr, "abuse-tool: unknown command `%s'\n", argv[2]);
+        return EXIT_FAILURE;
+    }
 
+    /* Check argument count and file access mode */
+    char const *mode = "rwb";
+    int minargc = 3;
+
+    switch (cmd)
+    {
+    case CMD_LIST:
+        mode = "rb"; // Read-only access
+        break;
+    case CMD_GET:
+        minargc = 4;
+        mode = "rb"; // Read-only access
+        break;
+    case CMD_PUT:
+        minargc = 5;
+        break;
+    case CMD_MOVE:
+        minargc = 5;
+        break;
+    case CMD_RENAME:
+        minargc = 5;
+        break;
+    case CMD_DEL:
+        minargc = 4;
+        break;
+    case CMD_GETPCX:
+        minargc = 4;
+        mode = "rb"; // Read-only access
+        break;
+    case CMD_PUTPCX:
+        minargc = 5;
+        break;
+    }
+
+    if (argc < minargc)
+    {
+        fprintf(stderr, "abuse-tool: too few arguments to command `%s'\n",
+                         argv[2]);
+        return EXIT_FAILURE;
+    }
+
+    /* Open the SPEC file */
+    char const *file = argv[1];
+
+    jFILE fp(file, mode);
+    if (fp.open_failure())
+    {
+        fprintf(stderr, "ERROR - could not open %s\n", file);
+        return EXIT_FAILURE;
+    }
+
+    spec_directory dir(&fp);
+
+    /* Now really execute commands */
+    if (cmd == CMD_LIST)
+    {
         printf("   id  type     size  name & information\n");
         printf(" ----  ----  -------  ----------------------------\n");
 
-        spec_directory dir(&fp);
         for (int i = 0; i < dir.total; i++)
         {
             spec_entry *se = dir.entries[i];
@@ -77,11 +149,31 @@ int main(int argc, char *argv[])
             /* Finish line */
             putchar('\n');
         }
+
+        return EXIT_SUCCESS;
     }
-    /* else if (...) */
-    else
+
+    if (cmd == CMD_GET)
     {
-        fprintf(stderr, "abuse-tool: unknown command `%s'\n", cmd);
+        int id = atoi(argv[3]);
+
+        if (id < 0 || id >= dir.total)
+        {
+            fprintf(stderr, "abuse-tool: id %i not found in %s\n", id, file);
+            return EXIT_FAILURE;
+        }
+
+        spec_entry *se = dir.entries[id];
+        fp.seek(se->offset, SEEK_SET);
+
+        for (size_t todo = se->size; todo > 0; )
+        {
+            uint8_t buf[1024];
+            int step = Min(todo, 1024);
+            fp.read(buf, step);
+            fwrite(buf, step, 1, stdout);
+            todo -= step;
+        }
     }
 
     return EXIT_SUCCESS;
@@ -92,6 +184,7 @@ static void Usage()
     fprintf(stderr, "%s",
             "Usage: abuse-tool <spec_file> <command> [args...]\n"
             "List of available commands:\n"
-            "  list -- list the contents of a SPEC file\n");
+            "  list          list the contents of a SPEC file\n"
+            "  get <id>      dump entry <id> to stdout\n");
 }
 
