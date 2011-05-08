bd76950abe40d389efbf214b9886361abaf2c7e5
sam <sam@1433025c-d204-0410-b9d4-f0ece44ced3d>
2011-05-08 15:32:32 +0000

    build: fix errors in the makefiles that prevent `make dist' from working.

--- abuse/trunk/src/imlib/visobj.h
+++ abuse/trunk/src/imlib/visobj.h
@@ -16,38 +16,12 @@
 
 class visual_object
 {
-  public :
-  virtual void draw(image *screen, int x, int y, Filter *f) = 0;
-  virtual int width() = 0;
-  virtual int height() = 0;
-  virtual ~visual_object() { }
-} ;
-
-
-
-class image_visual : public visual_object
-{
-  public :
-  image *im;
-
-  image_visual(image *img) { im=img; }
-  virtual void draw(image *screen, int x, int y, Filter *f);
-  virtual int width() { return im->Size().x; }
-  virtual int height() { return im->Size().y; }
-} ;
-
-
-class string_visual : public visual_object
-{
-  char *st;
-  int color;
-  int w,h;
-  public :
-  string_visual(char *string, int Color);
-  virtual void draw(image *screen, int x, int y, Filter *f);
-  virtual int width();
-  virtual int height();
-} ;
-
+public:
+    virtual void draw(image *screen, int x, int y, Filter *f) = 0;
+    virtual int width() = 0;
+    virtual int height() = 0;
+    virtual ~visual_object() { }
+};
 
 #endif
+
