8edb3b778328ed3dd590ac4d7e4aa1cc49ace702
sam <sam@1433025c-d204-0410-b9d4-f0ece44ced3d>
2011-05-10 18:31:57 +0000

    ps3: disable mouse support and the gamma selection widget on the PS3.

--- src/gamma.cpp
+++ src/gamma.cpp
@@ -151,6 +151,7 @@ void gamma_correct(palette *&pal, int force_menu)
 
         event ev;
         wm->flush_screen();
+#if !defined __CELLOS_LV2__
         do
         {
             do
@@ -165,6 +166,9 @@ void gamma_correct(palette *&pal, int force_menu)
         } while(!abort && (ev.type != EV_MESSAGE || ev.message.id != ID_GAMMA_OK));
 
         dg = ((spicker *)gw->inm->get(ID_GREEN_PICKER))->first_selected() * 4;
+#else
+        /* Do not display the gamma window on the PS3 for now. */
+#endif
 
         wm->close_window(gw);
         wm->flush_screen();
